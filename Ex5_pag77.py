3c751a35b72ad3e3543f3fec54ffa568214fc40e
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2021-11-22 09:33:36 +0200

    Update Ex5_pag77.py

--- Ex5_pag77.py
+++ Ex5_pag77.py
@@ -1,7 +1,4 @@
 with open('input.txt','r') as f:
     x=list(f.readline())
-j=0
-for i in x:
-    if i in ['a','o','u','i','e','ă']:
-        j+=1
-print('numarul de vocale in fisier:',j)
+j=[i for i in x if i in ['a','o','u','i','e','ă']]
+print('numarul de vocale in fisier:',len(j))
